0b4970e894928c92496cc5d7f93677763f379fc2
musagenius345 <musabadru@gmail.com>
2023-10-08 19:35:58 +0300

    7th stage I

--- app/main.js
+++ app/main.js
@@ -1,37 +1,51 @@
+const fs = require('fs');
+const path = require('path');
+
 const net = require('net');
 
 console.log('Logs from your program will appear here!');
 
-function extractPathAndUserAgent(requestString) {
-  const [startLine, ...headers] = requestString.split('\r\n');
-  const [, path] = startLine.split(' ');
-  const userAgentLine = headers.find((line) => line.startsWith('User-Agent: '));
-  const userAgent = userAgentLine ? userAgentLine.split(' ')[1] : '';
-  return { path, userAgent };
+const directoryFlagIndex = process.argv.indexOf('--directory');
+if (directoryFlagIndex === -1 || !process.argv[directoryFlagIndex + 1]) {
+  console.error('Error: Please provide a valid directory path using --directory flag.');
+  process.exit(1);
 }
+const directoryPath = process.argv[directoryFlagIndex + 1];
 
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
-    const { path, userAgent } = extractPathAndUserAgent(data.toString().trim());
+    const [method, requestPath] = extractMethodAndPath(data.toString().trim());
 
-    let response;
-    if (path === '/user-agent') {
-      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
-    } else if (path === '/') {
-      response = 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc';
-    } else if (path.startsWith('/echo/')) {
-      const randomString = path.substring('/echo/'.length);
-      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${randomString.length}\r\n\r\n${randomString}`;
+    if (method === 'GET' && requestPath.startsWith('/files/')) {
+      const filename = requestPath.substring('/files/'.length);
+      const filePath = path.join(directoryPath, filename);
+
+      if (fs.existsSync(filePath) && fs.statSync(filePath).isFile()) {
+        const fileContent = fs.readFileSync(filePath);
+        const contentLength = fileContent.length;
+
+        socket.write(
+          `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${contentLength}\r\n\r\n`
+        );
+        socket.write(fileContent, () => {
+          console.log('Response sent, connection closed');
+          socket.end();
+        });
+      } else {
+        socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+        socket.end();
+      }
     } else {
-      response = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 9\r\n\r\nNot Found';
-    }
-
-    socket.write(response, 'utf-8', () => {
-      console.log('Response sent, connection closed');
+      socket.write('HTTP/1.1 400 Bad Request\r\n\r\n');
       socket.end();
-    });
+    }
   });
 });
 
-server.listen(4221, 'localhost');
+function extractMethodAndPath(requestString) {
+  const [startLine] = requestString.split('\r\n');
+  const [method, path] = startLine.split(' ');
+  return [method, path];
+}
 
+server.listen(4221, 'localhost');
