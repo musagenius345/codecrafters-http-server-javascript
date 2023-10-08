77d9b59f229eeaf8b968abd450eb40195c841108
musagenius345 <musabadru@gmail.com>
2023-10-08 18:04:34 +0300

    pass 2nd stage II

--- app/main.js
+++ app/main.js
@@ -8,13 +8,15 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     log(data)
 
-      })
+    const response = 'HTTP/1.1 200 OK\r\n\r\n'
 
-  const response = 'HTTP/1.1 200 OK\r\n\r\n'
-
-    socket.write(response,'utf-8', () => {
+    socket.write(response, 'utf-8', () => {
       log('Response sent, connection closed')
     })
+
+    socket.end()
+    server.close()
+  })
 
   socket.on("close", () => {
     socket.end();
