53cf04d5bcc1e5f06608cd9baf98f0154077ddb8
musagenius345 <musabadru@gmail.com>
2023-10-08 19:46:14 +0300

    7th stage III

--- app/main.js
+++ app/main.js
@@ -1,59 +1,42 @@
 const net = require('net');
-const fs = require('fs').promises;
-const pathModule = require('path');
 
 console.log('Logs from your program will appear here!');
 
-function extractPath(requestString) {
-  const [startLine] = requestString.split('\r\n');
-  const [, path] = startLine.split(' ');
-  return path;
-}
-
-async function serveFile(socket, filePath) {
-  try {
-    const fileContent = await fs.readFile(filePath);
-    const contentLength = fileContent.length;
-
-    socket.write(`HTTP/1.1 200 OK\r\n`);
-    socket.write(`Content-Type: application/octet-stream\r\n`);
-    socket.write(`Content-Length: ${contentLength}\r\n\r\n`);
-    socket.write(fileContent, () => {
-      console.log('Response sent, connection closed');
-      socket.end();
-    });
-  } catch (error) {
-    console.error(error);
-    socket.write(`HTTP/1.1 404 Not Found\r\n\r\n`);
-    socket.end();
-  }
-}
-
 const server = net.createServer((socket) => {
-  socket.on('data', async (data) => {
-    const path = extractPath(data.toString().trim());
-    const basePath = '<directory>'; // Replace '<directory>' with the actual directory path
+  socket.on('data', (data) => {
+    const request = data.toString().trim();
 
-    if (path.startsWith('/files/')) {
-      const requestedFile = pathModule.join(basePath, path.substring('/files/'.length));
-
-      // Check if the requested file exists in the specified directory
-      try {
-        const stats = await fs.stat(requestedFile);
-        if (stats.isFile()) {
-          // Serve the file content as response
-          await serveFile(socket, requestedFile);
-          return;
-        }
-      } catch (error) {
-        console.error(error);
-      }
+    // Check if the request is empty or not a valid GET request
+    if (request !== 'GET /files/<filename> HTTP/1.1') {
+      socket.write('HTTP/1.1 400 Bad Request\r\n\r\n');
+      socket.end();
+      return;
     }
 
-    // If the requested file doesn't exist or invalid path, return a 404 response
-    socket.write(`HTTP/1.1 404 Not Found\r\n\r\n`);
-    socket.end();
+    // Replace '<filename>' with the actual filename to be served
+    const filePath = '<directory>/<filename>'; // Replace '<directory>' with the actual directory path
+    const statusCode = 200;
+
+    // Respond with HTTP/1.1 200 OK\r\n\r\n
+    socket.write(`HTTP/1.1 ${statusCode} OK\r\n\r\n`);
+    
+    // Read the file asynchronously and send its contents as the response body
+    require('fs').readFile(filePath, (err, data) => {
+      if (err) {
+        console.error(err);
+        socket.write('Error reading the file\r\n');
+      } else {
+        socket.write(data);
+      }
+
+      socket.end();
+    });
+  });
+
+  socket.on('close', () => {
+    console.log('Connection closed');
   });
 });
 
 server.listen(4221, 'localhost');
+
