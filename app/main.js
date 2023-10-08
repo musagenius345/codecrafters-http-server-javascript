5446628472255df4a3ea29234ea43882c481efe9
musagenius345 <musabadru@gmail.com>
2023-10-08 17:59:39 +0300

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -1,9 +1,21 @@
+const { log } = require("console");
 const net = require("net");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    log(data)
+
+      })
+
+  const response = 'HTTP/1.1 200 OK\r\n\r\n'
+
+    socket.write(response,'utf-8', () => {
+      log('Response sent, connection closed')
+    })
+
   socket.on("close", () => {
     socket.end();
     server.close();
