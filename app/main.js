58f2b2ce14a523a63dc03d7e9ad73a4bdebef540
musagenius345 <musabadru@gmail.com>
2023-10-08 20:35:53 +0300

    8th stage I

--- app/main.js
+++ app/main.js
@@ -1,65 +1,46 @@
 const fs = require('fs');
 const net = require('net');
-const path = require('path'); // Import the path module
+const path = require('path');
 
 console.log('Logs from your program will appear here!');
 
-function extractPathAndUserAgent(requestString) {
-  const [startLine, ...headers] = requestString.split('\r\n');
-  const [, path] = startLine.split(' ');
-  const userAgentLine = headers.find((line) => line.startsWith('User-Agent: '));
-  const userAgent = userAgentLine ? userAgentLine.split(' ')[1] : '';
-  return { path, userAgent };
-}
-
 const server = net.createServer((socket) => {
-  socket.on('data', (data) => {
-    const { path: rawPath, userAgent } = extractPathAndUserAgent(data.toString().trim());
+  let requestData = '';
 
-    if (rawPath.startsWith('/files/')) {
-      const directory = process.argv[3]; // Get the directory from command line arguments
-      const filename = rawPath.split('/files/')[1];
-      const filePath = path.join(directory, filename);
-      
-      if (fs.existsSync(filePath)) {
-        const fileContent = fs.readFileSync(filePath);
-        const response = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`;
+  socket.on('data', (data) => {
+    requestData += data.toString();
+
+    // Check if the request data includes the end of a POST request header
+    if (requestData.includes('\r\n\r\n')) {
+      const [headers, body] = requestData.split('\r\n\r\n');
+      const [startLine, ...headerLines] = headers.split('\r\n');
+      const [, method, path] = startLine.split(' ');
+
+      if (method === 'POST' && path.startsWith('/files/')) {
+        const directory = process.argv[3];
+        const filename = path.split('/files/')[1];
+        const filePath = path.join(directory, filename);
+
+        // Save the POST body (file content) to the specified file
+        fs.writeFileSync(filePath, body);
+
+        // Send a 201 Created response
+        const response = 'HTTP/1.1 201 Created\r\nContent-Length: 0\r\n\r\n';
         socket.write(response, 'utf-8', () => {
-          console.log('Response sent, connection closed');
+          console.log('File created, 201 response sent, connection closed');
           socket.end();
         });
       } else {
+        // Handle other request types here if needed
+        // ...
+
+        // Send a 404 Not Found response for unsupported routes
         const notFoundResponse = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 9\r\n\r\nNot Found';
         socket.write(notFoundResponse, 'utf-8', () => {
-          console.log('File not found, 404 response sent, connection closed');
+          console.log('Path not found, 404 response sent, connection closed');
           socket.end();
         });
       }
-    } else if (rawPath === '/user-agent') {
-      const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
-      socket.write(response, 'utf-8', () => {
-        console.log('Response sent, connection closed');
-        socket.end();
-      });
-    } else if (rawPath === '/') {
-      const response = 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc';
-      socket.write(response, 'utf-8', () => {
-        console.log('Response sent, connection closed');
-        socket.end();
-      });
-    } else if (rawPath.startsWith('/echo/')) {
-      const randomString = rawPath.substring('/echo/'.length);
-      const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${randomString.length}\r\n\r\n${randomString}`;
-      socket.write(response, 'utf-8', () => {
-        console.log('Response sent, connection closed');
-        socket.end();
-      });
-    } else {
-      const notFoundResponse = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 9\r\n\r\nNot Found';
-      socket.write(notFoundResponse, 'utf-8', () => {
-        console.log('Path not found, 404 response sent, connection closed');
-        socket.end();
-      });
     }
   });
 
