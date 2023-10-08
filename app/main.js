b94a2567da327c1f05f4b36e8bc677796ab08fc6
musagenius345 <musabadru@gmail.com>
2023-10-08 19:14:28 +0300

    5th stage IV

--- app/main.js
+++ app/main.js
@@ -2,24 +2,28 @@ const net = require('net');
 
 console.log('Logs from your program will appear here!');
 
-function extractPath(requestString) {
-  const [startLine] = requestString.split('\r\n');
+function extractPathAndUserAgent(requestString) {
+  const [startLine, ...headers] = requestString.split('\r\n');
   const [, path] = startLine.split(' ');
-  return path;
+  const userAgentLine = headers.find((line) => line.startsWith('User-Agent: '));
+  const userAgent = userAgentLine ? userAgentLine.split(' ')[1] : '';
+  return { path, userAgent };
 }
 
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
-    const path = extractPath(data.toString().trim());
+    const { path, userAgent } = extractPathAndUserAgent(data.toString().trim());
 
     let response;
-    if (path === '/') {
+    if (path === '/user-agent') {
+      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
+    } else if (path === '/') {
       response = 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc';
     } else if (path.startsWith('/echo/')) {
       const randomString = path.substring('/echo/'.length);
       response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${randomString.length}\r\n\r\n${randomString}`;
     } else {
-      response = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 17\r\n\r\nNot Found';
+      response = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 9\r\n\r\nNot Found';
     }
 
     socket.write(response, 'utf-8', () => {
@@ -30,4 +34,3 @@ const server = net.createServer((socket) => {
 });
 
 server.listen(4221, 'localhost');
-
