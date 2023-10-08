bc63d3e1fcbafd79c0617560ec0c727fc0ff6680
musagenius345 <musabadru@gmail.com>
2023-10-08 18:52:06 +0300

    pass 4th stage III

--- app/main.js
+++ app/main.js
@@ -1,29 +1,26 @@
 
-const net = require("net");
+const net = require('net');
 
-console.log("Logs from your program will appear here!");
+console.log('Logs from your program will appear here!');
 
 function extractPath(requestString) {
   const [startLine] = requestString.split('\r\n');
-  const [HTTPMethod, path, HTTPVersion] = startLine.split(' ');
+  const [, path] = startLine.split(' ');
   return path;
 }
 
-function extractEndpoint(path){
-  const parts =  path.split('/')
-  return parts[2] || ''
-}
-
 const server = net.createServer((socket) => {
-  socket.on("data", (data) => {
-    const requestPath = extractPath(data.toString().trim());
-    const endPoint = extractEndpoint(requestPath)
+  socket.on('data', (data) => {
+    const path = extractPath(data.toString().trim());
 
     let response;
-    if (endPoint) {
-      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${endPoint.length}\r\n\r\n${endPoint}`;
+    if (path === '/') {
+      response = 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc';
+    } else if (path.startsWith('/echo/')) {
+      const randomString = path.substring('/echo/'.length);
+      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${randomString.length}\r\n\r\n${randomString}`;
     } else {
-      response = 'HTTP/1.1 404 Not Found\r\n\r\n';
+      response = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 9\r\n\r\nNot Found';
     }
 
     socket.write(response, 'utf-8', () => {
@@ -33,5 +30,5 @@ const server = net.createServer((socket) => {
   });
 });
 
-server.listen(4221, "localhost");
+server.listen(4221, 'localhost');
 
