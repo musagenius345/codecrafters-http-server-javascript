e17fe92299d37e8a58d3947ce49131fd5fb73c7c
musagenius345 <musabadru@gmail.com>
2023-10-08 20:26:50 +0300

    7th stage XI

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const net = require('net');
-const path = require('path');
+const path = require('path'); // Import the path module
 
 console.log('Logs from your program will appear here!');
 
@@ -14,10 +14,10 @@ function extractPathAndUserAgent(requestString) {
 
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
-    const { path, userAgent } = extractPathAndUserAgent(data.toString().trim());
+    const { path: rawPath, userAgent } = extractPathAndUserAgent(data.toString().trim());
 
-    if (path.startsWith('/files/')) {
-      const filePath = path.substring('/files/'.length);
+    if (rawPath.startsWith('/files/')) {
+      const filePath = rawPath.substring('/files/'.length);
       const absolutePath = path.join(__dirname, filePath); // Constructing absolute path using path module
       if (fs.existsSync(absolutePath)) {
         const fileContent = fs.readFileSync(absolutePath);
@@ -25,12 +25,12 @@ const server = net.createServer((socket) => {
       } else {
         response = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 9\r\n\r\nNot Found';
       }
-    } else if (path === '/user-agent') {
+    } else if (rawPath === '/user-agent') {
       response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
-    } else if (path === '/') {
+    } else if (rawPath === '/') {
       response = 'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc';
-    } else if (path.startsWith('/echo/')) {
-      const randomString = path.substring('/echo/'.length);
+    } else if (rawPath.startsWith('/echo/')) {
+      const randomString = rawPath.substring('/echo/'.length);
       response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${randomString.length}\r\n\r\n${randomString}`;
     } else {
       response = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 9\r\n\r\nNot Found';
